233ffd3a4bbd465a284ca1b2b7add37170a906e0
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-04-10 16:45:38 +0530

    refactor: clean up App component and improve structure

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,13 @@ function App() {
   return (
     <>
       <section id="center">
+        <div>
+          <h1>let start with monitor</h1>
+        </div>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>let start with monitor</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
         </div>
         <button
           className="counter"
@@ -58,7 +55,6 @@ function App() {
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
@@ -72,20 +68,9 @@ function App() {
                 GitHub
               </a>
             </li>
+
             <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
+              <a href="https://x.com/DipanshuVi6799" target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -94,18 +79,6 @@ function App() {
                   <use href="/icons.svg#x-icon"></use>
                 </svg>
                 X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
               </a>
             </li>
           </ul>
